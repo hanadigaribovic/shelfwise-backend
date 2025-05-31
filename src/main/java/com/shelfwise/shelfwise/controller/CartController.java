522a303b778df9a4d0309d74b208297a7788ef1e
hanadigaribovic <118663134+hanadigaribovic@users.noreply.github.com>
2025-06-01 00:49:45 +0200

    Update CartController.java

--- src/main/java/com/shelfwise/shelfwise/controller/CartController.java
+++ src/main/java/com/shelfwise/shelfwise/controller/CartController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/api/cart")
 public class CartController {
 
     private final CartService cartService;
