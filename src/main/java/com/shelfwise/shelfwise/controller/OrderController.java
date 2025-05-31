2907982508012f47ea03dd8065bdf15229b3a716
hanadigaribovic <118663134+hanadigaribovic@users.noreply.github.com>
2025-06-01 00:50:01 +0200

    Update OrderController.java

--- src/main/java/com/shelfwise/shelfwise/controller/OrderController.java
+++ src/main/java/com/shelfwise/shelfwise/controller/OrderController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/api/order")
 public class OrderController {
 
     private final OrderService orderService;
