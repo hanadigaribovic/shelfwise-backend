c104a4c0ceff75ffe1afeac8b80b86270b083606
hanadigaribovic <118663134+hanadigaribovic@users.noreply.github.com>
2025-06-01 10:26:39 +0200

    UPDATED: CorsConfig.java

--- src/main/java/com/shelfwise/shelfwise/config/CorsConfig.java
+++ src/main/java/com/shelfwise/shelfwise/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200",
+            "https://shelfwise-bookstore.netlify.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
